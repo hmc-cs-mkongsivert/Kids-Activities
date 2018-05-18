4a624416d2b18ace885c01a8209076a1b0996b53
hmc-cs-hamster <mkongsivert@hmc.edu>
2018-05-18 14:52:22 -0700

    scraper pulls events and locations but only pulls one event and leaves in too much whitespace

--- newseum-scrape.py
+++ newseum-scrape.py
@@ -1,35 +1,38 @@
+import requests
+
+site = requests.get("http://www.newseum.org/events-programs/")
 searchTerm = '"ai1ec-event"'
 
 def between(string, start, beginTag, endTag):
 	begin = string.find(beginTag, start) + len(beginTag)
-	end = string.find(endTag, start)
+	end = string.find(endTag, begin)
 	return string[begin:end]
 
-with open('newseum.html') as site:
-	siteText = site.read()
-	#remove whitespace
-	siteText.replace(' ', '')
-	siteText.replace('\t', '')
-	siteText.replace('\n', '')
+#remove whitespace
+siteText = site.text
+siteText.replace("\t", "")
+siteText.replace("\n", "")
 
-	s = 0
-	indices = []
-	while True:
-		newI = siteText.find(searchTerm, s)
-		if newI == -1:
-			break
-		s += len(searchTerm)
-		indices.append(newI)
-		print(newI)
+s = 0
+indices = []
+while True:
+	newI = siteText.find(searchTerm, s)
+	if newI == -1:
+		break
+	s += len(searchTerm)
+	indices.append(newI)
 
-indices = findEvents('newseum.html')
+table = []
 for i in [0]+indices[:-1]:
-	title = between(siteText, i, '<spanclass="ai1ec-event-title">', '</span>')
+	title = between(siteText, i, '<span class="ai1ec-event-title">', '</span>')
 	location = between(siteText, i, '<span class="ai1ec-event-location">', '</span>')
-	'''
+	table.append([title, location])
+
+print(table)
+'''
 	TODO: I think there is a bug where I don't consider multiple instances of
 	the end tag, so I need to fix that in the between function. I also need to
 	find somewhere to put the titles and locations, like in a CSV file.
 	Finally, I need to include othe tags than title and location and do some
 	general cleaning up.
-	'''
+'''
