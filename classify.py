4d2a5939c94161cdfb36cbad54322381a76059ce
hmc-cs-mkongsivert <mkongsivert@cs.hmc.edu>
2018-05-31 10:59:53 -0700

    code to load samples

--- classify.py
+++ classify.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-#import tensorflow_hub as hub
+import tensorflow_hub as hub
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -13,12 +13,27 @@ import csv
 def load_data(file):
 	data = {}
 	data["sentence"] = []
-	data["sentiment"] = []
+	data["age-group"] = []
 	with open(file, newline='') as csvFile:
 		dataReader = csv.reader(csvFile)
 		for row in dataReader:
 			data["sentence"].append(row[0])
-			data["sentiment"].append(float(row[1]))
+			data["age-group"].append(float(row[1]))
 	return pd.DataFrame.from_dict(data)
 
 #build one training and one testing dataset
+def load_dataset(filename):
+	young_df = load_data(filename + "-young.csv")
+	mid_df = load_data(filename + "-mid.csv")
+	old_df = load_data(filename + "-old.csv")
+	young_df["polarity"] = 0
+	mid_df = ["polarity"] = 0.5
+	old_df = ["polarity"] = 1
+	return pd.concat([young_df, mid_df, old_df]).sample(frac=1).reset_index(drop=True)
+
+def dowload_and_load_datasets(force_download=False):
+	train_df = load_dataset("train")
+	test_df = load_dataset("test")
+
+	return train_df, test_df
+
