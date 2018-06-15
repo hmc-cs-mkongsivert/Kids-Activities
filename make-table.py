fd551a97fae5b947f5346f448212701dc5f7fa17
hmc-cs-mkongsivert <mkongsivert@cs.hmc.edu>
2018-06-15 11:31:42 -0700

    main function wraps up mapmaking

--- make-table.py
+++ make-table.py
@@ -27,9 +27,9 @@ def makeDict():
 
 #TODO: add javascript
 def makeTable(mapLabels):
-	'''takes in a dictionary of events separated by location and returns an
+	'''takes in a dictionary of events indexed by location and returns an
 	HTML-formatted table displaying event times and titles'''
-	lb = '\n' + '\t'*11 #indenting to match the rest
+	lb = '\n' + '\t'*7 #indenting to match the rest
 	tableStr = lb+'<table>'
 	for key in mapLabels.keys():
 		tableStr += lb+'<div id="'+key[:5].lower()'">'+lb+'<h3>'+key+'</h3>'
@@ -42,8 +42,42 @@ def makeTable(mapLabels):
 		tableStr += lb+'</table>'+lb
 	return tableStr
 
-def makeMap(mapLabels):
-	'''takes in a dictionary of events separated by location and returns a
+def makeJS(mapLabels):
+	'''takes in a dictionary of events indexed by location and returns the
+	javascript to hide and show the corresponding table of events'''
+	lb = '\n'+'\t'*7
+
+	#construct map
+	scriptStr="<script>"+lb+"var map = L.map('map').setView([38.89,"
+	scriptStr+=" -77.026148], 11);"+lb+"L.tileLayer('https://api.tiles.mapbox"
+	scriptStr+=".com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {"+lb
+	scriptStr+="attribution: 'Map data &copy; <a href=\"https://www."
+	scriptStr+="openstreetmap.org/\">OpenStreetMap</a> contributors, <a href="
+	scriptStr+="\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA"
+	scriptStr+="</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>"
+	scriptStr+="',"+lb+"maxZoom: 20,"+lb+"id: 'mapbox.streets',"+lb+"access"
+	scriptStr+="Token: 'pk.eyJ1IjoibWtvbmdzaXZlcnQiLCJhIjoiY2ppNHljYTZlMGViYTN"
+	scriptStr+="ybzY1ODBrZHFteiJ9.cryeQAatX8rCKMgGo8rRNw'"+lb+"}).addTo(map);"
+
+	for key in mapLabels.keys():
+		keyID = key[:5].lower()
+		#set the table's visibility to hidden initially
+		scriptStr+=ls*2+"var "+keyID+"Table = document.getElementById('"+keyID
+		scriptStr+="');"+lb+keyID"Table.style.visibility = 'hidden';"
+		#create functions to change tables' visibilities
+		scriptStr+=ls*2+"function "+keyID+"In(e) {"+lb+keyID+"Table.style."
+		scriptStr+="visibility = 'visible';"+lb+"}"+lb+"function "+keyID
+		scriptStr+="Out(e) {"+lb+keyID+"Table.style.visibility = 'hidden';"+lb+"}"
+		#add interaction FIX-----------------------------------------------------------------------------------
+		scriptStr+="function onEachFeature(feature, layer) {"+lb+"if (feature."
+		scriptStr+="properties && feature.properties.popupContent) {"+lb
+		scriptStr+="popupContent = feature.properties.popupContent;"+lb+"}"
+		scriptStr+=lb+"layer.bindPopup(popupContent);"+lb+"layer.on({"
+		scriptStr+=lb+"mouseover: sidebarIn,"+lb+"mouseout: sidebarOut"+lb
+		scriptStr+="});"+lb+"}"
+
+def makeJSON(mapLabels):
+	'''takes in a dictionary of events indexed by location and returns a
 	GeoJSON file to indicate location and quantity of events on a map'''
 	jsonStr = ""
 	for key in mapLabels.keys():
@@ -59,23 +93,21 @@ def makeMap(mapLabels):
 		jsonStr += str(shape)+']\n]\n]\n}\n};\n\n'
 	return jsonStr
 
-#OVERHAUL
-def writeHTML(beginTag, endTag, filename, function):
-	'''inserts what a given function indicates should be inserted in filename
-	between beginTag and endTag'''
-	with open(filename, 'r', newline='') as htmlRead:
+def main():
+	'''reads in events from CSV file and puts them into the HTML and JSON
+	files'''
+	beginTag = "<!-- Tables Begin Here -->"
+	endTag = "<!-- Tables End Here -->"
+	eventDict = makeDict()
+	with open('geojson.js', 'w', newline='') as jsonWrite:
+		jsonStr = makeJSON(eventDict)
+		jsonWrite.write(jsonStr)
+	with open('map.html', 'r', newline='') as htmlRead:
 		oldText = htmlRead.read()
-		table = function(mapLabels)
+		tables = makeTable(mapLabels)
+		script = makeJS(mapLabels)
 		beforeTable = oldText.find(beginTag) + len(beginTag)
 		afterTable = oldText.find(endTag)
-		newText = oldText[:beforeTable] + table + oldText[afterTable:]
-	with open(filename, 'w', newline='') as htmlWrite:
+		newText = oldText[:beforeTable] + tables + script + oldText[afterTable:]
+	with open('map.html', 'w', newline='') as htmlWrite:
 		htmlWrite.write(newText)
-
-begTable = "<!-- Table Begins Here -->"
-endTable = "<!-- Table Ends Here -->"
-#writeHTML(begTable, endTable, 'map.html', makeTable)
-
-begMap = "//begin labels"
-endMap = "//end labels"
-writeHTML(begMap, endMap, 'map.html', makeMap)
