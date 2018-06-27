31953860b0465062d8586ae2132bd50aef276343
hmc-cs-mkongsivert <mkongsivert@cs.hmc.edu>
2018-06-27 11:13:53 -0700

    debugged

--- tagtest.py
+++ tagtest.py
@@ -1,11 +1,21 @@
 import sys
+from helpertools import *
 
-with open(sys.argv, 'r', newline='') as htmlFile:
+exempt = ['br', 'meta', 'link', 'rect', 'polygon']
+
+with open(sys.argv[1], 'r') as filename:
+	htmlFile = removeWhitespace(filename.read())
 	i = 0
 	tagDict = {}
-	while i != -1:
+	while i > -1:
 		beg = htmlFile.find('<', i)
-		end = min([htmlFile.find(' ', i), htmlFile.find('>', i)])
+		
+		if htmlFile.find(' ', beg) == -1:
+			end = htmlFile.find('>', beg)
+		elif htmlFile.find('>', beg) == -1:
+			end = htmlFile.find(' ', beg)
+		else:
+			end = min([htmlFile.find(' ', beg), htmlFile.find('>', beg)])
 		tag = htmlFile[beg+1: end]
 		i = end
 
@@ -18,7 +28,9 @@ with open(sys.argv, 'r', newline='') as htmlFile:
 				tagDict[tag]+=1
 			else:
 				tagDict[tag] =1
+		if (len(htmlFile)-end) <= 1:
+			break
 
 for key in tagDict.keys():
-	if tagDict[key] != 0:
-		print(key+': '+tagDict[key])
+	if tagDict[key] != 0 and key not in exempt:
+		print(key+': '+str(tagDict[key]))
